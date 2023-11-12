a09a7e6f504609f075ad54b6c4e2305b3815f049
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-13 02:30:04 +0900

    [FEAT] Delimiter 관련 예외 처리 구현
    .

--- src/main/java/christmas/utils/ChristmasUtils.java
+++ src/main/java/christmas/utils/ChristmasUtils.java
@@ -6,16 +6,27 @@ import java.util.List;
 
 public class ChristmasUtils {
     private static final String DELIMITER = ",";
+    private static final String SPACE = " ";
 
     public static int stringToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR]");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다. 다시 입력해 주세요.");
         }
     }
 
     private static List<String> parseToList(String input) {
+        validateDelimiter(input);
         return Arrays.asList(input.split(DELIMITER));
+    }
+
+    private static void validateDelimiter(String input) {
+        if (input.startsWith(DELIMITER) || input.endsWith(DELIMITER)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다. 다시 입력해 주세요.");
+        }
+        if (input.contains(DELIMITER + DELIMITER) || input.contains(DELIMITER + SPACE)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다. 다시 입력해 주세요.");
+        }
     }
 }
