c7a76572712811eb30e5a43c787d92fa3c6163b2
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-13 02:25:43 +0900

    [FEAT] Util에 ","기준으로 나누는 함수 생성
    .

--- src/main/java/christmas/utils/ChristmasUtils.java
+++ src/main/java/christmas/utils/ChristmasUtils.java
@@ -1,11 +1,21 @@
 package christmas.utils;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+
 public class ChristmasUtils {
-    public int stringToInt(String input) {
+    private static final String DELIMITER = ",";
+
+    public static int stringToInt(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR]");
         }
+    }
+
+    private static List<String> parseToList(String input) {
+        return Arrays.asList(input.split(DELIMITER));
     }
 }
