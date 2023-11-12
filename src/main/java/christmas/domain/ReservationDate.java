300d61b30b4126507d40b55dea1e52cc13ab168b
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-13 02:17:06 +0900

    [FEAT] 1부터 31까지 숫자 외에는 예외처리 구현
    .

--- src/main/java/christmas/domain/ReservationDate.java
+++ src/main/java/christmas/domain/ReservationDate.java
@@ -1,10 +1,19 @@
 package christmas.domain;
 
 public class ReservationDate {
+    private final int EVENT_START_DAY = 1;
+    private final int EVENT_END_DAY = 31;
     private final int date;
 
     public ReservationDate(int date) {
+        checkEventPeriod(date);
         this.date = date;
+    }
+
+    private void checkEventPeriod(int date) {
+        if (date < EVENT_START_DAY || date > EVENT_END_DAY) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
     public int getReservationDate() {
