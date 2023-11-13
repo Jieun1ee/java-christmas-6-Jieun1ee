c2f75830dfda63226cd8f1f86dad0321d7942080
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-13 14:51:21 +0900

    [REFACTOR] InputView.java static 선언
    .

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,17 +3,17 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private final String VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private final String MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public String visitDate() {
+    public static String visitDate() {
         System.out.println(START_MESSAGE);
         System.out.println(VISIT_DATE);
         return Console.readLine();
     }
 
-    public String menuOrder() {
+    public static String menuOrder() {
         System.out.println(MENU);
         return Console.readLine();
     }
