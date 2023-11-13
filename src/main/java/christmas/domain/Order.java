e0cd5c89b8d8d6b071947ba1532574182c073125
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 00:55:59 +0900

    [FEAT] Order.java 구현

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,11 @@
+package christmas.domain;
+
+import christmas.Menu;
+
+public class Order {
+    private Menu menu;
+    private int quantity;
+    public Order(Menu menu, int quantity) {
+        
+    }
+}
