5a73a583bb11d1e78cc8c62b2cbfd1601db76160
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-13 14:50:19 +0900

    [FEAT] 메뉴를 저장하는 menuList 구현
    메뉴 이름 중복과, 없는 메뉴를 입력 한 경우도 예외처리.
    예외처리 문구는 수정예정

--- src/main/java/christmas/domain/OrderMenu.java
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -1,12 +1,36 @@
 package christmas.domain;
 
 import christmas.Menu;
+import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderMenu {
     EnumMap<Menu, Integer> menuQuantity;
 
     public OrderMenu() {
         menuQuantity = new EnumMap<>(Menu.class);
+    }
+
+    // depth : 2
+    public EnumMap<Menu, Integer> menuList(List<String> parseToList) {
+        return parseToList.stream()
+                .map(item -> item.split("-"))
+                .collect(Collectors.toMap(
+                        parts -> getMenuName(parts[0]),
+                        parts -> Integer.parseInt(parts[1]),
+                        (existing, replacement) -> {
+                            throw new IllegalStateException("[ERROR] 중복 ");
+                        },
+                        () -> new EnumMap<>(Menu.class)
+                ));
+    }
+
+    private Menu getMenuName(String menuName) {
+        return Arrays.stream(Menu.values())
+                .filter(menu -> menu.menuName.equals(menuName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 여기는 없는 값을 입력하면, 유효하지 않은 주문입니다. 다시 입력해 주세요."));
     }
 }
