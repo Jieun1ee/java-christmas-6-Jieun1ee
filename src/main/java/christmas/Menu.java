2c77c6fbc58ee1f7062cb63490b2839bee295457
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 15:26:02 +0900

    [REFACTOR] Menu 중 티본스테이크 이름 수정
    .

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -4,7 +4,7 @@ public enum Menu {
     MUSHROOM_SOUP("Appetizer", "양송이수프", 6000),
     TAPAS("Appetizer", "타파스", 5500),
     CAESAR_SALAD("Appetizer", "시저샐러드", 8000),
-    TBONE_STEAK("Main", "티본스테이크", 55000),
+    T_BONE_STEAK("Main", "티본스테이크", 55000),
     BARBECUE_RIBS("Main", "바비큐립", 54000),
     SEAFOOD_PASTA("Main", "해산물파스타", 35000),
     CHRISTMAS_PASTA("Main", "크리스마스파스타", 25000),
