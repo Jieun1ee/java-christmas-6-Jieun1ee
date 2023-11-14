7e81777160916147ddf9d2710e269de599b1549d
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 23:44:53 +0900

    [FEAT] 샴페인 증정 이벤트 기능 구현
    .

---
+++ src/main/java/christmas/domain/event/GiftEvent.java
@@ -0,0 +1,22 @@
+package christmas.domain.event;
+
+public class GiftEvent {
+    private final int GIFT_EVENT_THRESHOLD = 120000;
+    private final int CHAMPAGNE_PRICE = 25000;
+    private final int NO_DISCOUNT = 0;
+    private int discount;
+
+    public GiftEvent(int totalCost) {
+        this.discount = calculateGiftEvent(totalCost);
+    }
+    private int calculateGiftEvent(int totalCost) {
+        if (applyGiveawayEvent(totalCost)) {
+            return CHAMPAGNE_PRICE;
+        }
+        return NO_DISCOUNT;
+    }
+
+    private boolean applyGiveawayEvent(int totalCost) {
+        return totalCost >= GIFT_EVENT_THRESHOLD;
+    }
+}
