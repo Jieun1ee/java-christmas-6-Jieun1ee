353bf8072dcf80aad32426e66c45d59dfd5bed40
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 12:30:32 +0900

    [REFACTOR] OrderMenu.java의 getMenuName 함수 이동
    .

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,11 +1,21 @@
 package christmas.domain;
 
 import christmas.Menu;
+import java.util.Arrays;
 
 public class Order {
-    private Menu menu;
-    private int quantity;
-    public Order(Menu menu, int quantity) {
-        
+    private final Menu menu;
+    private final int quantity;
+
+    public Order(String menu, int quantity) {
+        this.menu = getMenuName(menu);
+        this.quantity = quantity;
+    }
+
+    private Menu getMenuName(String menuName) {
+        return Arrays.stream(Menu.values())
+                .filter(menu -> menu.menuName.equals(menuName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 여기는 없는 값을 입력하면, 유효하지 않은 주문입니다. 다시 입력해 주세요."));
     }
 }
