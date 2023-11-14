c1b7a456b022bad056cefd742164ba85bae81397
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 12:32:47 +0900

    [REFACTOR] createTotalOrderMenu로 함수 이름 변경, 구조 분리
    .

--- src/main/java/christmas/domain/OrderMenu.java
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -1,37 +1,30 @@
 package christmas.domain;
 
-import christmas.Menu;
-import java.util.Arrays;
-import java.util.EnumMap;
+import christmas.utils.ChristmasUtils;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OrderMenu {
-    EnumMap<Menu, Integer> orderList;
+    private final List<Order> totalOrder;
 
-    public OrderMenu() {
-        orderList = new EnumMap<>(Menu.class);
+    public OrderMenu(String input) {
+        this.totalOrder = createTotalOrderMenu(input);
     }
 
-    // depth : 2 -> 함수 분리 해야 함
-    public EnumMap<Menu, Integer> menuList(List<String> parseToList) {
-        return parseToList.stream()
-                .map(item -> item.split("-"))
-                .collect(Collectors.toMap(
-                        parts -> getMenuName(parts[0]),
-                        parts -> Integer.parseInt(parts[1]),
-                        (existing, replacement) -> {
-                            throw new IllegalStateException("[ERROR] 중복 ");
-                        },
-                        () -> new EnumMap<>(Menu.class)
-                ));
+    public List<Order> createTotalOrderMenu(String input) {
+        List<String> orders = ChristmasUtils.splitWithComma(input);
+
+        return orders.stream()
+                .map(order -> {
+                    List<String> orderComponents = ChristmasUtils.splitWithHypen(order);
+                    String menuName = orderComponents.get(0);
+                    int menuQuantity = ChristmasUtils.stringQuantityToInt(orderComponents.get(1));
+                    return createOrder(menuName, menuQuantity);
+                })
+                .collect(Collectors.toList());
     }
 
-    // 메쏘드 명 수정 하고 싶음
-    private Menu getMenuName(String menuName) {
-        return Arrays.stream(Menu.values())
-                .filter(menu -> menu.menuName.equals(menuName))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 여기는 없는 값을 입력하면, 유효하지 않은 주문입니다. 다시 입력해 주세요."));
+    public Order createOrder(String menuName, int menuCount) {
+        return new Order(menuName, menuCount);
     }
 }
