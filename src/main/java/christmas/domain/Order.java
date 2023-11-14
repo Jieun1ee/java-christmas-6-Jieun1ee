ca4a81791ee8386f380b267986d253c859354806
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 12:31:19 +0900

    [FEAT]  각각의 수량을 확인해서 예외처리하는 함수 구현
    .

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -8,6 +8,7 @@ public class Order {
     private final int quantity;
 
     public Order(String menu, int quantity) {
+        validateQuantity(quantity);
         this.menu = getMenuName(menu);
         this.quantity = quantity;
     }
@@ -17,5 +18,24 @@ public class Order {
                 .filter(menu -> menu.menuName.equals(menuName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 여기는 없는 값을 입력하면, 유효하지 않은 주문입니다. 다시 입력해 주세요."));
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return menu.toString();
     }
 }
