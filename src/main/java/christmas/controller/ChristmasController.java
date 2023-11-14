92291c8f42a019e33557d7e64ca80b6d439ff3e7
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-14 12:36:26 +0900

    [FEAT] controller에서 예약 날짜를 받는 함수 구현
    .

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,7 +1,20 @@
 package christmas.controller;
 
+import christmas.domain.Order;
+import christmas.domain.OrderMenu;
+import christmas.domain.ReservationDate;
+import christmas.utils.ChristmasUtils;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.List;
+
 public class ChristmasController {
     public void run() {
+        ReservationDate reservationDate = inputReservationDate();
+        int date = reservationDate.getReservationDate();
+    }
 
+    private ReservationDate inputReservationDate() {
+        return new ReservationDate(ChristmasUtils.stringDateToInt(InputView.visitDate()));
     }
 }
