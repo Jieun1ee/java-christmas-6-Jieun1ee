88bff795185cda5622922ccce71c35070e233395
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-15 21:16:42 +0900

    [TEST] DecemberDiscount 계산 테스트 구현

---
+++ src/test/java/christmas/domain/event/DecemberDiscountTest.java
@@ -0,0 +1,39 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import christmas.domain.order.Order;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class DecemberDiscountTest {
+    private static Stream<Arguments> weekdayOrderArguments() {
+        return Stream.of(
+                Arguments.of(5, List.of(new Order("초코케이크", 1), new Order("초코케이크", 1)), 15000)
+        );
+    }
+    @DisplayName("평일 할인")
+    @MethodSource("weekdayOrderArguments")
+    @ParameterizedTest
+    void calculateWeekdayDiscount(int date, List<Order> totalOrder, int totalCost) {
+        int discount = new DecemberDiscount(date, totalOrder ,totalCost).getDiscount();
+        assertThat(discount).isEqualTo(4046);
+    }
+
+    private static Stream<Arguments> weekendOrderArguments() {
+        return Stream.of(
+                Arguments.of(1, List.of(new Order("티본스테이크", 1), new Order("초코케이크", 1)), 15000)
+        );
+    }
+    @DisplayName("평일 할인")
+    @MethodSource("weekendOrderArguments")
+    @ParameterizedTest
+    void calculateWeekendDiscount(int date, List<Order> totalOrder, int totalCost) {
+        int discount = new DecemberDiscount(date, totalOrder ,totalCost).getDiscount();
+        assertThat(discount).isEqualTo(2023);
+    }
+}
