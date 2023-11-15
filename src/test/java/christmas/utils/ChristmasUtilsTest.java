d77373a84366f4650558c1a7f1d7c3da3fa299b0
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-15 18:08:50 +0900

    [TEST] ChristmasUtils에 대한 성공 케이스 테스트 구현
    .

--- src/test/java/christmas/utils/ChristmasUtilsTest.java
+++ src/test/java/christmas/utils/ChristmasUtilsTest.java
@@ -1,7 +1,9 @@
 package christmas.utils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -39,5 +41,43 @@ class ChristmasUtilsTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("숫자가 아닌 수량 입력에 대한 예외 처리")
+    @ValueSource(strings = {"a", "ㄱ", "*"})
+    @ParameterizedTest
+    void checkStrigQuantityToInt(String input) {
+        assertThatThrownBy(() -> ChristmasUtils.stringQuantityToInt(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("Comma로 split 성공")
+    @ValueSource(strings = {"타파스-1,제로콜라-1"})
+    @ParameterizedTest
+    void splitOrderWithComma(String input) {
+        List<String> splitResult = ChristmasUtils.splitWithComma(input);
+        assertThat(splitResult).containsExactly("타파스-1", "제로콜라-1");
+    }
+
+    @DisplayName("Hypen으로 split 성공")
+    @ValueSource(strings = {"타파스-1"})
+    @ParameterizedTest
+    void splitOrderWithHypen(String input) {
+        List<String> splitResult = ChristmasUtils.splitWithHypen(input);
+        assertThat(splitResult).containsExactly("타파스", "1");
+    }
+
+    @DisplayName("parseToInt - date")
+    @ValueSource(strings = {"25"})
+    @ParameterizedTest
+    void parseDateInt(String input) {
+        int date = ChristmasUtils.stringDateToInt(input);
+        assertThat(date).isEqualTo(25);
+    }
+
+    @DisplayName("parseToInt - quantity")
+    @ValueSource(strings = {"2"})
+    @ParameterizedTest
+    void parseQuantityInt(String input) {
+        int quantity = ChristmasUtils.stringQuantityToInt(input);
+        assertThat(quantity).isEqualTo(2);
+    }
 }
