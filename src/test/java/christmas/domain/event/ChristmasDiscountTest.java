9aaaaca3b7af2041505e96f5bb386e8c20329a34
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-15 21:08:44 +0900

    [TEST] ChristmasDiscount 계산에 대한 테스트 구현

---
+++ src/test/java/christmas/domain/event/ChristmasDiscountTest.java
@@ -0,0 +1,28 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.order.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ChristmasDiscountTest {
+    @DisplayName("1일부터 25일 사이에 크리스마스 디데이 할인")
+    @CsvSource("25, 15000")
+    @ParameterizedTest
+    void calculateDiscount(int date, int totalCost) {
+        int discount = new ChristmasDiscount(date, totalCost).getDiscount();
+        assertThat(discount).isEqualTo(3400);
+    }
+
+    @DisplayName("1일부터 25일 사이에 크리스마스 디데이 할인")
+    @CsvSource("25, 1000")
+    @ParameterizedTest
+    void calculateNoDiscount(int date, int totalCost) {
+        int discount = new ChristmasDiscount(date, totalCost).getDiscount();
+        assertThat(discount).isEqualTo(0);
+    }
+}
